8634a573232627e4f326a2eb133b387bac8d5c7d
olehhapuk <oleggapuk20@gmail.com>
2020-07-22 22:48:50 +0300

    feat: created seeder for users and places

---
+++ server/admin/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../db');
+const faker = require('faker');
+const { fake } = require('faker');
+
+//#region places
+
+router.get('/places', function(req, res) {
+  pool.query('SELECT * FROM places', function(error, results) {
+    if (error) throw error;
+    res.json(results);
+  });
+});
+
+router.get('/places/:id', function(req, res) {
+  pool.query(`SELECT * FROM places WHERE id='${req.params.id}'`, function(error, results) {
+    res.json(results[0]);
+  });
+});
+
+router.post('/places/add', function(req, res) {
+  pool.query(`INSERT INTO places (name, adress, latlng, description) VALUES ('${req.body.name}', '${req.body.adress}', '${req.body.latlng}', '${req.body.description}')`,
+    function(error, results, fields) {
+      if (error) throw error;
+      res.json({message: 'Place added successfully'});
+    }
+  );
+});
+
+router.patch('/places/:id', function(req, res) {
+  pool.query(`SELECT id FROM places WHERE id='${req.params.id} LIMIT 1'`, function (error, results) {
+    if (results.length <= 0) {
+      res.json({message: `Error: No place with id ${req.params.id} found`});
+    }
+    else {
+      pool.query(`UPDATE places SET name='${req.body.name}', adress='${req.body.adress}', latlng='${req.body.latlng}', description='${req.body.latlng}' WHERE id='${req.params.id}'`, function(error, results) {
+        if (error) throw error;
+        else {
+          res.json({message: 'Place updated successfully'});
+        }
+      });
+    }
+  });
+});
+
+//#endregion
+
+//#region seeds
+
+router.post('/seed/users', (req, res) => {
+  const count = req.body.count;
+  for (let i = 0; i < count; i++) {
+    pool.query(`INSERT INTO users (username) VALUES ('${faker.internet.userName()}')`, (error, results) => {
+      if (error) throw error;
+    });
+  }
+  res.json({message: 'Users created sucessfully'});
+});
+
+router.post('/seed/places', (req, res) => {
+  const count = req.body.count;
+  for (let i = 0; i < count; i++) {
+    pool.query(`INSERT INTO places (name, adress, latlng, thumbnail, description) VALUES ('${faker.address.streetName()}', '${faker.address.streetAddress()}', '${faker.address.latitude()},${faker.address.longitude()}', '${faker.image.imageUrl()}', '${faker.lorem.paragraph(10)}')`, (error, results) => {
+      if (error) throw error;
+    });
+  }
+  res.json({message: 'Places created sucessfully'});
+});
+
+//#endregion
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const volleyball = require('volleyball');
 const pool = require('./db');
 
+const admin = require('./admin');
+
 require('dotenv').config();
 
 app.use(express.json());
@@ -12,51 +14,6 @@ app.use(cors({
 }));
 app.use(volleyball);
 
-// app.post('/', function(req, res) {
-//   connection.connect();
-//   connection.query('SELECT * FROM users', function (error, results, fields) {
-//     if (error) throw error;
-//     res.json(results);
-//   });
-//   connection.end();
-// });
-
-app.get('/admin/places', function(req, res) {
-  pool.query('SELECT * FROM places', function(error, results) {
-    if (error) throw error;
-    res.json(results);
-  });
-});
-
-app.get('/admin/places/:id', function(req, res) {
-  pool.query(`SELECT * FROM places WHERE id='${req.params.id}'`, function(error, results) {
-    res.json(results[0]);
-  });
-});
-
-app.post('/admin/places/add', function(req, res) {
-  pool.query(`INSERT INTO places (name, adress, latlng, description) VALUES ('${req.body.name}', '${req.body.adress}', '${req.body.latlng}', '${req.body.description}')`,
-    function(error, results, fields) {
-      if (error) throw error;
-      res.json({message: 'Place added successfully'});
-    }
-  );
-});
-
-app.patch('/admin/places/:id', function(req, res) {
-  pool.query(`SELECT id FROM places WHERE id='${req.params.id} LIMIT 1'`, function (error, results) {
-    if (results.length <= 0) {
-      res.json({message: `Error: No place with id ${req.params.id} found`});
-    }
-    else {
-      pool.query(`UPDATE places SET name='${req.body.name}', adress='${req.body.adress}', latlng='${req.body.latlng}', description='${req.body.latlng}' WHERE id='${req.params.id}'`, function(error, results) {
-        if (error) throw error;
-        else {
-          res.json({message: 'Place updated successfully'});
-        }
-      });
-    }
-  });
-});
+app.use('/admin', admin);
 
 app.listen(process.env.APP_PORT, () => console.log(`Server is running at port ${process.env.APP_PORT}`));
