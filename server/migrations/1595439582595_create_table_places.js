0b4bdc0cce644013606f29b4435d2155120313e1
olehhapuk <oleggapuk20@gmail.com>
2020-07-22 22:56:50 +0300

    feat: removed images column from places table

--- server/migrations/1595439582595_create_table_places.js
+++ server/migrations/1595439582595_create_table_places.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "up": "CREATE TABLE `places` (`id` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(535) NOT NULL, `adress` varchar(535) NOT NULL, `latlng` varchar(64) NOT NULL, `thumbnail` varchar(535) NOT NULL, `images` text NOT NULL, `description` text NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4",
+    "up": "CREATE TABLE `places` (`id` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(535) NOT NULL, `adress` varchar(535) NOT NULL, `latlng` varchar(64) NOT NULL, `thumbnail` varchar(535) NOT NULL, `description` text NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4",
     "down": "DROP TABLE places"
 }
