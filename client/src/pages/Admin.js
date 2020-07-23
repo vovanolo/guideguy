42bee3ee24832ffb1db4ca899cb32cd3fecbc128
vovanolo <vovchik.student221@gmail.com>
2020-07-23 16:35:42 +0300

    addmin added tables and render

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -7,16 +7,19 @@ export default class Admin extends React.Component {
 
     this.state = {
       serverHost: 'http://localhost:3050',
+      id: '',
       name: '',
       adress: '',
       description: '',
-      latlng: ''
+      latlng: '',
+      places: []
     }
+
   }
 
   AddPlace(e) {
     e.preventDefault();
-    axios.post(`${this.state.serverHost}/admin/places/add`, {
+    axios.post(`${this.state.serverHost}/admin/places`, {
       name: this.state.name,
       adress: this.state.adress,
       latlng: this.state.latlng,
@@ -24,17 +27,65 @@ export default class Admin extends React.Component {
     }).then(res => console.log(res.data));
   }
 
+  ViewPlace() {
+    axios.get(`${this.state.serverHost}/admin/places`).then(res => this.setState({places: res.data}))
+  }
+
+  // DeletePlace() {
+  //   axios.delete(`${this.state.serverHost}/admin/places${this.state.id}`).
+  // }
+
+  componentDidMount(){
+    this.ViewPlace();
+  }
+
   render() {
     return (
       <div className="container ml-auto">
+        <h1>Admin</h1>
         <form onSubmit={e => this.AddPlace(e)}>
-          <input type="text" defaultValue="eee" placeholder="Name" onChange={e => this.setState({name: e.currentTarget.value})} />
-          <input type="text" placeholder="Adress" onChange={e => this.setState({adress: e.currentTarget.value})} />
+          <input type="text" className="form-control" defaultValue="eee" placeholder="Name" onChange={e => this.setState({name: e.currentTarget.value})} />
+          <input type="text" className="form-control" placeholder="Adress" onChange={e => this.setState({adress: e.currentTarget.value})} />
           <br />
-          <input type="text" placeholder="Lat. Lng." onChange={e => this.setState({latlng: e.currentTarget.value})} />
+          <input type="text" className="form-control" placeholder="Lat. Lng." onChange={e => this.setState({latlng: e.currentTarget.value})} />
           <textarea type="text" placeholder="Description" onChange={e => this.setState({description: e.currentTarget.value})} />
-          <input type="submit" value="Додати місце" />
+          <input type="file" placeholder="photo" />
+
+          <input type="submit" className="btn btn-primary" value="Додати місце" />
+
+         
+         
+
         </form>
+        
+        <table class="table table-dark">
+        <thead>
+                  <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">adress</th>
+                    <th scope="col">latlang</th>
+                  </tr>
+                </thead>
+            {this.state.places.map((place, index) => (
+              // <li key={index}>{place.name}</li>
+                <tbody>
+                  <tr>
+                    <th key={index} scope="row">{place.id}</th>
+                    <td key={index}>{place.name}</td>
+                    <td key={index}>{place.adress}</td>
+                    <td key={index}>{place.latlng}</td>
+                    <td><button className="btn btn-warning">Edit</button></td>
+                    <td><button className="btn btn-danger">Delete</button></td>
+                  </tr>
+                </tbody>
+            ))}
+        </table>
+
+
+        <button onClick={() => axios.post(`${this.state.serverHost}/admin/seed/places`, {count:5})} className="btn btn-success">Go dani</button>
+
+
       </div>
     );
   }
