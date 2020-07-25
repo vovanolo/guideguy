be8f6803cd04144b94faa9bccc6d4a2edd0fc6e6
olehhapuk <oleggapuk20@gmail.com>
2020-07-25 20:08:02 +0300

    feat: now place can be deleted

--- server/places/index.js
+++ server/places/index.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const pool = require('../db');
 const CheckAdmin = require('../middlewares/CheckAdmin');
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   pool.query('SELECT * FROM places', function(error, results) {
     if (error) throw error;
     res.json(results);
   });
 });
 
-router.get('/:id', function(req, res) {
+router.get('/:id', (req, res) => {
   pool.query(`SELECT * FROM places WHERE id='${req.params.id}'`, function(error, results) {
     res.json(results[0]);
   });
 });
 
-router.post('/', CheckAdmin, function(req, res) {
+router.post('/', CheckAdmin, (req, res) => {
   pool.query(`INSERT INTO places (name, adress, latlng, description) VALUES ('${req.body.name}', '${req.body.adress}', '${req.body.latlng}', '${req.body.description}')`,
     function(error, results, fields) {
       if (error) throw error;
@@ -25,7 +25,7 @@ router.post('/', CheckAdmin, function(req, res) {
   );
 });
 
-router.patch('/:id', CheckAdmin, function(req, res) {
+router.patch('/:id', CheckAdmin, (req, res) => {
   pool.query(`SELECT id FROM places WHERE id='${req.params.id} LIMIT 1'`, function (error, results) {
     if (results.length <= 0) {
       res.json({message: `Error: No place with id ${req.params.id} found`});
@@ -35,6 +35,22 @@ router.patch('/:id', CheckAdmin, function(req, res) {
         if (error) throw error;
         else {
           res.json({message: 'Place updated successfully'});
+        }
+      });
+    }
+  });
+});
+
+router.delete('/:id', CheckAdmin, (req, res) => {
+  pool.query(`SELECT id FROM places WHERE id='${req.params.id} LIMIT 1'`, (error, results) => {
+    if (results.length <= 0) {
+      res.json({message: `Error: No place with id ${req.params.id} found`});
+    }
+    else {
+      pool.query(`DELETE FROM places WHERE id='${req.params.id}'`, (error, results) => {
+        if (error) throw error;
+        else {
+          res.json({message: 'Place deleted successfully'});
         }
       });
     }
