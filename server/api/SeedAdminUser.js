99e663a5298b0ec51c3d548399bc461efc35cef5
olehhapuk <oleggapuk20@gmail.com>
2020-07-31 09:57:47 +0300

    fix: pool closing in SeedAminUser.js

--- server/api/SeedAdminUser.js
+++ server/api/SeedAdminUser.js
@@ -1,5 +1,6 @@
-const pool = require('../db');
 const bcrypt = require('bcrypt');
+
+const pool = require('../db');
 
 require('dotenv').config();
 
@@ -7,6 +8,7 @@ pool.query(`SELECT id FROM users WHERE role='admin' LIMIT 1`, (error, data) => {
   if (error) throw error;
   if (data.length > 0) {
     console.log('Admin user already exists.');
+    pool.end(err => err && console.log(err));
   }
   else {
     bcrypt.hash(process.env.ADMIN_PASSWORD, 10, (error, hashed) => {
@@ -14,9 +16,8 @@ pool.query(`SELECT id FROM users WHERE role='admin' LIMIT 1`, (error, data) => {
       pool.query(`INSERT INTO users (username, password, role) VALUES ('${process.env.ADMIN_USERNAME}', '${hashed}', 'admin')`, (error, data) => {
         if (error) throw error;
         console.log('Admin user created');
+        pool.end(err => err && console.log(err));
       });
-      pool.end(err => err && console.log(err));
     });
   }
-  pool.end(err => err && console.log(err));
 });
