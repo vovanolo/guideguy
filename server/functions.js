5c9e4f31584b69db6498e0abf5971f7fa7c33e82
olehhapuk <oleggapuk20@gmail.com>
2020-08-06 12:39:41 +0300

    feat: minor changes

--- server/functions.js
+++ server/functions.js
@@ -1,7 +1,5 @@
 function throwError(res, next, error, statusCode) {
-    const newError = new Error(error);
-    res.status(statusCode);
-    next(newError);
+    next(error);
 }
 
 module.exports = { throwError };
